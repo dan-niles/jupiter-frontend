84fe45ec966772fb3e6f0f6247c3575428a1253c
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2022-11-26 09:52:24 +0530

    Add login authentication

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 // @mui
 import {
 	Link,
@@ -9,6 +11,7 @@ import {
 	TextField,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
+
 // components
 import Iconify from "../../../components/iconify";
 
@@ -17,58 +20,83 @@ import Iconify from "../../../components/iconify";
 export default function LoginForm() {
 	const navigate = useNavigate();
 
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
 	const [showPassword, setShowPassword] = useState(false);
 
-	const handleClick = () => {
-		navigate("/dashboard", { replace: true });
+	const onFormSubmit = (e) => {
+		e.preventDefault();
+
+		axios
+			.post("http://localhost:8080/api/login/", {
+				username: username,
+				password: password,
+			})
+			.then((response) => {
+				console.log(response);
+				if (response.data.username == username) {
+					navigate("/dashboard", { replace: true });
+				}
+			})
+			.catch((response) => {
+				console.log(response);
+			});
 	};
 
 	return (
 		<>
-			<Stack spacing={3}>
-				<TextField name="username" label="Username" />
+			<form onSubmit={onFormSubmit}>
+				<Stack spacing={3}>
+					<TextField
+						name="username"
+						label="Username"
+						value={username}
+						onChange={(e) => {
+							setUsername(e.target.value);
+						}}
+					/>
 
-				<TextField
-					name="password"
-					label="Password"
-					type={showPassword ? "text" : "password"}
-					InputProps={{
-						endAdornment: (
-							<InputAdornment position="end">
-								<IconButton
-									onClick={() => setShowPassword(!showPassword)}
-									edge="end"
-								>
-									<Iconify
-										icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
-									/>
-								</IconButton>
-							</InputAdornment>
-						),
-					}}
-				/>
-			</Stack>
+					<TextField
+						name="password"
+						label="Password"
+						type={showPassword ? "text" : "password"}
+						value={password}
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position="end">
+									<IconButton
+										onClick={() => setShowPassword(!showPassword)}
+										edge="end"
+									>
+										<Iconify
+											icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
+										/>
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+				</Stack>
 
-			<Stack
-				direction="row"
-				alignItems="flex-end"
-				justifyContent="flex-end"
-				sx={{ mt: 2, mb: 4 }}
-			>
-				<Link variant="subtitle2" underline="hover">
-					Forgot password?
-				</Link>
-			</Stack>
+				<Stack
+					direction="row"
+					alignItems="flex-end"
+					justifyContent="flex-end"
+					sx={{ mt: 2, mb: 4 }}
+				>
+					<Link variant="subtitle2" underline="hover">
+						Forgot password?
+					</Link>
+				</Stack>
 
-			<LoadingButton
-				fullWidth
-				size="large"
-				type="submit"
-				variant="contained"
-				onClick={handleClick}
-			>
-				Login
-			</LoadingButton>
+				<LoadingButton fullWidth size="large" type="submit" variant="contained">
+					Login
+				</LoadingButton>
+			</form>
 		</>
 	);
 }
