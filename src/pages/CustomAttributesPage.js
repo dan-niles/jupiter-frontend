b9278cef21629c25c774f1d0ceeda5f0ee67875c
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-04 10:00:07 +0530

    Add edit functionality for custom attributes

--- src/pages/CustomAttributesPage.js
+++ src/pages/CustomAttributesPage.js
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet-async";
 import { NavLink as RouterLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-// @mui
+import toast, { Toaster } from "react-hot-toast";
 import {
 	Card,
 	Table,
@@ -49,12 +49,42 @@ export default function CustomAttributesPage() {
 
 	const [customAttributes, setCustomAttributes] = useState([]);
 
-	const handleClickOpen = () => {
+	const [editId, setEditId] = useState(null);
+	const [editAlias, setEditAlias] = useState("");
+
+	const handleClickOpen = (id, idx) => {
+		setEditAlias(customAttributes[idx].alias);
+		setEditId(id);
 		setOpen(true);
 	};
 
 	const handleClose = () => {
 		setOpen(false);
+	};
+
+	const handleEdit = (e) => {
+		e.preventDefault();
+		axios
+			.put(
+				process.env.REACT_APP_BACKEND_URL + "/api/custom_attributes/" + editId,
+				{
+					headers: {
+						"access-token": `${accessToken}`,
+					},
+					data: {
+						attr_id: editId,
+						alias: editAlias,
+					},
+				}
+			)
+			.then((res) => {
+				toast.success("Edited successfully!");
+			})
+			.catch((err) => {
+				toast.error("Error editing value!");
+			});
+		handleClose();
+		getCustomAttributes();
 	};
 
 	useEffect(() => {
@@ -79,6 +109,8 @@ export default function CustomAttributesPage() {
 			<Helmet>
 				<title> Custom Attributes | Jupiter HRM </title>
 			</Helmet>
+
+			<Toaster position="top-right" reverseOrder={true} />
 
 			<Container>
 				<Stack
@@ -122,7 +154,14 @@ export default function CustomAttributesPage() {
 											<TableCell align="center">{row.data_type}</TableCell>
 											<TableCell align="center">{row.alias}</TableCell>
 											<TableCell align="center">
-												<IconButton aria-label="edit" onClick={handleClickOpen}>
+												<IconButton
+													aria-label="edit"
+													onClick={handleClickOpen.bind(
+														this,
+														row.attr_id,
+														index
+													)}
+												>
 													<Iconify icon={"eva:edit-fill"} />
 												</IconButton>
 												<IconButton
@@ -144,41 +183,30 @@ export default function CustomAttributesPage() {
 
 			<Dialog open={open} onClose={handleClose}>
 				<DialogTitle>Edit</DialogTitle>
-				<DialogContent>
-					<DialogContentText>
-						Enter the new values for the selected attribute.
-					</DialogContentText>
-					<Stack spacing={2} direction="column" sx={{ mt: 2 }}>
-						<TextField
-							id="column_name"
-							label="Column Name"
-							type="text"
-							fullWidth
-							// sx={{ mb: 1 }}
-						/>
-						<TextField
-							id="type"
-							select
-							label="Data Type"
-							sx={{ width: "25ch" }}
-						>
-							<MenuItem key="varchar" value="varchar">
-								VARCHAR
-							</MenuItem>
-							<MenuItem key="int" value="int">
-								INTEGER
-							</MenuItem>
-							<MenuItem key="date" value="date">
-								DATE
-							</MenuItem>
-						</TextField>
-						<TextField id="alias" label="Alias" type="text" fullWidth />
-					</Stack>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose}>Cancel</Button>
-					<Button onClick={handleClose}>Save</Button>
-				</DialogActions>
+				<form onSubmit={handleEdit}>
+					<DialogContent>
+						<DialogContentText>
+							Enter the new alias for the selected attribute.
+						</DialogContentText>
+						<Stack spacing={2} direction="column" sx={{ mt: 2 }}>
+							<TextField
+								required
+								id="alias"
+								label="Alias"
+								type="text"
+								fullWidth
+								value={editAlias}
+								onChange={(e) => {
+									setEditAlias(e.target.value);
+								}}
+							/>
+						</Stack>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Cancel</Button>
+						<Button type="submit">Save</Button>
+					</DialogActions>
+				</form>
 			</Dialog>
 		</>
 	);
