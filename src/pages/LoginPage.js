92c12bfc40f3c0f172b46774559e21dedf019953
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-06 05:26:07 +0530

    Refactor login page component and display error messages for invalid credentials

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,26 @@
 import { Helmet } from "react-helmet-async";
 import { styled } from "@mui/material/styles";
 import { Container, Typography, Box } from "@mui/material";
-import { LoginForm } from "../sections/auth/login";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { useContext } from "react";
+import UserContext from "../context/user-context";
+import toast, { Toaster } from "react-hot-toast";
+
+// @mui
+import {
+	Link,
+	Stack,
+	IconButton,
+	InputAdornment,
+	TextField,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
+// components
+import Iconify from "../components/iconify";
 
 const StyledRoot = styled("div")(({ theme }) => ({
 	[theme.breakpoints.up("xs")]: {
@@ -24,12 +43,56 @@ const StyledContent = styled("div")(({ theme }) => ({
 
 // ----------------------------------------------------------------------
 
+const setAuthToken = (token) => {
+	if (token) {
+		axios.defaults.headers.common["access-token"] = `Bearer ${token}`;
+	} else delete axios.defaults.headers.common["access-token"];
+};
+
 export default function LoginPage() {
+	const navigate = useNavigate();
+	const userContext = useContext(UserContext);
+
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const [showPassword, setShowPassword] = useState(false);
+
+	const onFormSubmit = (e) => {
+		e.preventDefault();
+
+		axios
+			.post(process.env.REACT_APP_BACKEND_URL + "/api/login/", {
+				username: username,
+				password: password,
+			})
+			.then((response) => {
+				if (response.data.username == username) {
+					delete response.data.password;
+					sessionStorage.setItem("user-data", JSON.stringify(response.data));
+					sessionStorage.setItem("access-token", response.data.token);
+					// setAuthToken(response.data.token);
+					userContext.setData(response.data);
+					navigate("/dashboard", { replace: true });
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				if (err.response.status === 404) {
+					toast.error("Invalid credentials!");
+				} else {
+					toast.error("Something went wrong!");
+				}
+			});
+	};
+
 	return (
 		<>
 			<Helmet>
 				<title> Login | Jupiter HRM </title>
 			</Helmet>
+
+			<Toaster position="top-right" reverseOrder={true} />
 
 			<StyledRoot>
 				<Container maxWidth="sm">
@@ -59,7 +122,64 @@ export default function LoginPage() {
 							Use your Jupiter Account
 						</Typography>
 
-						<LoginForm />
+						<form onSubmit={onFormSubmit}>
+							<Stack spacing={3}>
+								<TextField
+									name="username"
+									label="Username"
+									value={username}
+									onChange={(e) => {
+										setUsername(e.target.value);
+									}}
+								/>
+
+								<TextField
+									name="password"
+									label="Password"
+									type={showPassword ? "text" : "password"}
+									value={password}
+									onChange={(e) => {
+										setPassword(e.target.value);
+									}}
+									InputProps={{
+										endAdornment: (
+											<InputAdornment position="end">
+												<IconButton
+													onClick={() => setShowPassword(!showPassword)}
+													edge="end"
+												>
+													<Iconify
+														icon={
+															showPassword ? "eva:eye-fill" : "eva:eye-off-fill"
+														}
+													/>
+												</IconButton>
+											</InputAdornment>
+										),
+									}}
+								/>
+							</Stack>
+
+							<Stack
+								direction="row"
+								alignItems="flex-end"
+								justifyContent="flex-end"
+								sx={{ mt: 2, mb: 4 }}
+							>
+								<Link variant="subtitle2" underline="hover">
+									Forgot password?
+								</Link>
+							</Stack>
+
+							<LoadingButton
+								fullWidth
+								size="large"
+								type="submit"
+								variant="contained"
+							>
+								Login
+							</LoadingButton>
+						</form>
 					</StyledContent>
 				</Container>
 			</StyledRoot>
