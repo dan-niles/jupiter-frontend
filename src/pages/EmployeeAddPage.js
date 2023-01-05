1900652cdf5b31a2b72115b763bc204f3f5c714d
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-06 03:49:31 +0530

    Add functionality to create employees

--- src/pages/EmployeeAddPage.js
+++ src/pages/EmployeeAddPage.js
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet-async";
-import { useState } from "react";
-import { NavLink as RouterLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { NavLink as RouterLink, useNavigate } from "react-router-dom";
 // @mui
 import {
 	Card,
@@ -17,15 +17,164 @@ import {
 import Iconify from "../components/iconify";
 import Scrollbar from "../components/scrollbar";
 import { Box } from "@mui/system";
+import { format } from "date-fns";
 
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import axios from "axios";
 
 // ----------------------------------------------------------------------
 
+const accessToken = sessionStorage.getItem("access-token");
+
 export default function EmployeeAddPage() {
-	const [value, setValue] = useState(null);
+	const [empID, setEmpID] = useState(null);
+	const [fullName, setFullName] = useState(null);
+	const [firstName, setFirstName] = useState(null);
+	const [lastName, setLastName] = useState(null);
+	const [email, setEmail] = useState(null);
+	const [nic, setNIC] = useState(null);
+	const [birthdate, setBirthDate] = useState(null);
+	const [paygradeID, setPaygradeID] = useState(null);
+	const [contractID, setContractID] = useState(null);
+	const [departmentID, setDepartmentID] = useState(null);
+	const [titleID, setTitleID] = useState(null);
+	const [statusID, setStatusID] = useState(null);
+	const [maritalStatus, setMaritalStatus] = useState(null);
+	const [supervisorID, setSupervisorID] = useState(null);
+
+	const [paygrades, setPaygrades] = useState([]);
+	const [contracts, setContracts] = useState([]);
+	const [statuses, setStatuses] = useState([]);
+	const [titles, setTitles] = useState([]);
+	const [departments, setDepartments] = useState([]);
+	const [supervisors, setSupervisors] = useState([]);
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		getPaygrades();
+		getContracts();
+		getTitles();
+		getStatuses();
+		getDepartments();
+		getSupervisors();
+	}, []);
+
+	const getPaygrades = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/paygrade/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setPaygrades(res.data);
+			});
+	};
+
+	const getContracts = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/contract/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setContracts(res.data);
+			});
+	};
+
+	const getTitles = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/title/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setTitles(res.data);
+			});
+	};
+
+	const getStatuses = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/status/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setStatuses(res.data);
+			});
+	};
+
+	const getDepartments = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/department/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setDepartments(res.data);
+			});
+	};
+
+	const getSupervisors = () => {
+		axios
+			.get(process.env.REACT_APP_BACKEND_URL + "/api/employee/", {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				setSupervisors(res.data);
+			});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const data = {
+			emp_id: empID,
+			full_name: fullName,
+			first_name: firstName,
+			last_name: lastName,
+			email: email,
+			nic: nic,
+			birthdate: format(birthdate, "yyyy-MM-dd"),
+			paygrade_id: paygradeID,
+			contract_id: contractID,
+			dept_id: departmentID,
+			title_id: titleID,
+			status_id: statusID,
+			supervisor_id: supervisorID,
+			marital_status: maritalStatus,
+		};
+		console.log(data);
+		axios
+			.post(process.env.REACT_APP_BACKEND_URL + "/api/employee/", data, {
+				headers: {
+					"access-token": `${accessToken}`,
+				},
+			})
+			.then((res) => {
+				console.log(res.data);
+				navigate("/dashboard/employee/", {
+					state: {
+						showToast: true,
+						toastMessage: "Employee created successfully!",
+					},
+				});
+			});
+	};
 
 	return (
 		<>
@@ -74,7 +223,7 @@ export default function EmployeeAddPage() {
 								Add New Employee
 							</Typography>
 						</Stack>
-						<form>
+						<form onSubmit={handleSubmit}>
 							<Grid container spacing={2}>
 								<Grid item xs={6}>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -82,8 +231,10 @@ export default function EmployeeAddPage() {
 											required
 											id="emp_id"
 											label="Employee ID"
-											// value={name}
-											// onChange={handleChange}
+											value={empID}
+											onChange={(e) => {
+												setEmpID(e.target.value);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -92,8 +243,10 @@ export default function EmployeeAddPage() {
 											id="full_name"
 											label="Full Name"
 											sx={{ width: "50ch" }}
-											// value={name}
-											// onChange={handleChange}
+											value={fullName}
+											onChange={(e) => {
+												setFullName(e.target.value);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -102,25 +255,30 @@ export default function EmployeeAddPage() {
 											id="first_name"
 											label="First Name"
 											sx={{ width: "24ch" }}
-											// value={name}
-											// onChange={handleChange}
+											value={firstName}
+											onChange={(e) => {
+												setFirstName(e.target.value);
+											}}
 										/>
 										<TextField
 											required
 											id="last_name"
 											label="Last Name"
 											sx={{ width: "24ch" }}
-											// value={name}
-											// onChange={handleChange}
+											value={lastName}
+											onChange={(e) => {
+												setLastName(e.target.value);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={3} sx={{ mb: 2 }}>
 										<LocalizationProvider dateAdapter={AdapterDateFns}>
 											<DatePicker
+												required
 												label="Birthdate"
-												value={value}
+												value={birthdate}
 												onChange={(newValue) => {
-													setValue(newValue);
+													setBirthDate(newValue);
 												}}
 												renderInput={(params) => <TextField {...params} />}
 											/>
@@ -130,8 +288,10 @@ export default function EmployeeAddPage() {
 											id="email"
 											label="Email"
 											sx={{ width: "25ch" }}
-											// value={name}
-											// onChange={handleChange}
+											value={email}
+											onChange={(e) => {
+												setEmail(e.target.value);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -140,22 +300,36 @@ export default function EmployeeAddPage() {
 											id="nic"
 											label="NIC"
 											sx={{ width: "25ch" }}
-											// value={name}
-											// onChange={handleChange}
+											value={nic}
+											onChange={(e) => {
+												setNIC(e.target.value);
+											}}
 										/>
 										<TextField
+											required
 											id="marital_status"
 											select
 											label="Marital Status"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={maritalStatus}
+											onChange={(e) => {
+												setMaritalStatus(e.target.value);
+											}}
 										>
-											<MenuItem key="Single" value="Single">
+											<MenuItem key="single" value="single">
 												Single
 											</MenuItem>
-											<MenuItem key="Married" value="Married">
+											<MenuItem key="married" value="married">
 												Married
+											</MenuItem>
+											<MenuItem key="widowed" value="widowed">
+												Widowed
+											</MenuItem>
+											<MenuItem key="divorced" value="divorced">
+												Divorced
+											</MenuItem>
+											<MenuItem key="seperated" value="seperated">
+												Seperated
 											</MenuItem>
 										</TextField>
 									</Stack>
@@ -163,33 +337,43 @@ export default function EmployeeAddPage() {
 								<Grid item xs={6}>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
 										<TextField
+											required
 											id="title"
 											select
 											label="Title"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={titleID}
+											onChange={(e) => {
+												setTitleID(e.target.value);
+											}}
 										>
-											<MenuItem
-												key="Software Engineer"
-												value="Software Engineer"
-											>
-												Software Engineer
-											</MenuItem>
+											{titles.map((title) => (
+												<MenuItem key={title.title_id} value={title.title_id}>
+													{title.job_title}
+												</MenuItem>
+											))}
 										</TextField>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
 										<TextField
+											required
 											id="department"
 											select
 											label="Department"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={departmentID}
+											onChange={(e) => {
+												setDepartmentID(e.target.value);
+											}}
 										>
-											<MenuItem key="ICT" value="ICT">
-												ICT
-											</MenuItem>
+											{departments.map((department) => (
+												<MenuItem
+													key={department.dept_id}
+													value={department.dept_id}
+												>
+													{department.dept_name}
+												</MenuItem>
+											))}
 										</TextField>
 
 										<TextField
@@ -197,56 +381,87 @@ export default function EmployeeAddPage() {
 											select
 											label="Paygrade"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											required
+											value={paygradeID}
+											onChange={(e) => {
+												setPaygradeID(e.target.value);
+											}}
 										>
-											<MenuItem key="Level 1" value="Level 1">
-												Level 1
-											</MenuItem>
-											<MenuItem key="Level 2" value="Level 2">
-												Level 2
-											</MenuItem>
+											{paygrades.map((row) => {
+												return (
+													<MenuItem
+														key={row.paygrade_id}
+														value={row.paygrade_id}
+													>
+														{row.level}
+													</MenuItem>
+												);
+											})}
 										</TextField>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
 										<TextField
+											required
 											id="status"
 											select
 											label="Status"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={statusID}
+											onChange={(e) => {
+												setStatusID(e.target.value);
+											}}
 										>
-											<MenuItem key="Fulltime" value="Fulltime">
-												Full-time
-											</MenuItem>
+											{statuses.map((row) => {
+												return (
+													<MenuItem key={row.status_id} value={row.status_id}>
+														{row.type}
+													</MenuItem>
+												);
+											})}
 										</TextField>
 
 										<TextField
+											required
 											id="contract"
 											select
 											label="Contract"
 											sx={{ width: "25ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={contractID}
+											onChange={(e) => {
+												setContractID(e.target.value);
+											}}
 										>
-											<MenuItem key="Fulltime" value="Fulltime">
-												Full-time
-											</MenuItem>
+											{contracts.map((row) => {
+												return (
+													<MenuItem
+														key={row.contract_id}
+														value={row.contract_id}
+													>
+														{row.type}
+													</MenuItem>
+												);
+											})}
 										</TextField>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
 										<TextField
+											required
 											id="supervisor_id"
 											select
 											label="Supervisor"
 											sx={{ width: "30ch" }}
-											// value={currency}
-											// onChange={handleChange}
+											value={supervisorID}
+											onChange={(e) => {
+												setSupervisorID(e.target.value);
+											}}
 										>
-											<MenuItem key="John Doe" value="John Doe">
-												John Doe
-											</MenuItem>
+											{supervisors.map((row) => {
+												return (
+													<MenuItem key={row.emp_id} value={row.emp_id}>
+														{row.first_name + " " + row.last_name}
+													</MenuItem>
+												);
+											})}
 										</TextField>
 									</Stack>
 								</Grid>
