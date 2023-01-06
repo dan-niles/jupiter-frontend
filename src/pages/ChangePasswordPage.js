121976e26c175146652f87d73610c32d4ce7df19
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-06 18:05:36 +0530

    Implement change password functionality

--- src/pages/ChangePasswordPage.js
+++ src/pages/ChangePasswordPage.js
@@ -1,57 +1,85 @@
 import { Helmet } from "react-helmet-async";
 import { useState } from "react";
-import { NavLink as RouterLink } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 // @mui
 import {
 	Card,
 	Stack,
-	Paper,
 	Button,
 	Container,
 	Typography,
 	TextField,
-	MenuItem,
 	Grid,
 } from "@mui/material";
 // components
-import Iconify from "../components/iconify";
 import Scrollbar from "../components/scrollbar";
 import { Box } from "@mui/system";
 
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
+import { useContext } from "react";
+import UserContext from "../context/user-context";
+
+const accessToken = sessionStorage.getItem("access-token");
 
 // ----------------------------------------------------------------------
 
 export default function ChangePasswordPage() {
+	const [oldPassword, setOldPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const [oldPasswordError, setOldPasswordError] = useState(false);
+
+	const userContext = useContext(UserContext);
+	const user_id = userContext.userData.user_id;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (newPassword !== confirmPassword) {
+			toast.error("Passwords do not match!");
+		} else {
+			axios
+				.put(
+					process.env.REACT_APP_BACKEND_URL +
+						"/api/user/change-password/" +
+						user_id,
+					{
+						headers: {
+							"access-token": `${accessToken}`,
+						},
+						data: {
+							user_id: user_id,
+							old_password: oldPassword,
+							new_password: newPassword,
+						},
+					}
+				)
+				.then((res) => {
+					setOldPassword("");
+					setNewPassword("");
+					setConfirmPassword("");
+					toast.success("Changed password successfully!");
+				})
+				.catch((err) => {
+					if (err.response.data.message === "INVALID_CURR_PW") {
+						setOldPasswordError(true);
+						toast.error("Current Password is Invalid!");
+					} else {
+						toast.error("Something went wrong!");
+					}
+				});
+		}
+	};
+
 	return (
 		<>
 			<Helmet>
 				<title> Change Password | Jupiter HRM </title>
 			</Helmet>
 
+			<Toaster position="top-right" reverseOrder={true} />
+
 			<Container>
-				{/* <Stack
-					direction="row"
-					alignItems="center"
-					justifyContent="space-between"
-					mb={5}
-				>
-					<Typography variant="h4" gutterBottom>
-						Change Password
-					</Typography>
-
-					<Button
-						color="error"
-						variant="outlined"
-						startIcon={<Iconify icon="eva:arrow-ios-back-fill" />}
-						component={RouterLink}
-						to="/dashboard"
-					>
-						Go Back
-					</Button>
-				</Stack> */}
-
 				<Card>
 					<Box
 						sx={{
@@ -71,7 +99,7 @@ export default function ChangePasswordPage() {
 								Change Password
 							</Typography>
 						</Stack>
-						<form>
+						<form onSubmit={handleSubmit}>
 							<Grid container spacing={2}>
 								<Grid item xs={5}>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -80,6 +108,15 @@ export default function ChangePasswordPage() {
 											label="Current Password"
 											sx={{ width: "50ch" }}
 											type="password"
+											helperText={
+												oldPasswordError ? "Current password is invalid" : ""
+											}
+											error={oldPasswordError}
+											value={oldPassword}
+											onChange={(e) => {
+												setOldPassword(e.target.value);
+												setOldPasswordError(false);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
@@ -88,14 +125,28 @@ export default function ChangePasswordPage() {
 											label="New Password"
 											sx={{ width: "50ch" }}
 											type="password"
+											value={newPassword}
+											onChange={(e) => {
+												setNewPassword(e.target.value);
+											}}
 										/>
 									</Stack>
 									<Stack direction="row" spacing={2} sx={{ mb: 2 }}>
 										<TextField
+											error={newPassword !== confirmPassword}
+											helperText={
+												newPassword !== confirmPassword
+													? "Passwords do not match"
+													: ""
+											}
 											id="confirm_password"
 											label="Confirm New Password"
 											sx={{ width: "50ch" }}
 											type="password"
+											value={confirmPassword}
+											onChange={(e) => {
+												setConfirmPassword(e.target.value);
+											}}
 										/>
 									</Stack>
 								</Grid>
