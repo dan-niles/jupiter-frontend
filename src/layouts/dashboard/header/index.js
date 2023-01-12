e9a33c99daaa9230fa23557981a668abfd38545e
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-01-12 13:02:02 +0530

    Remove search icon from header

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -59,7 +59,7 @@ export default function Header({ onOpenNav }) {
 					<Iconify icon="eva:menu-2-fill" />
 				</IconButton>
 
-				<Searchbar />
+				{/* <Searchbar /> */}
 				<Box sx={{ flexGrow: 1 }} />
 
 				<Stack
